2c9c18f0e5438d9c45f763dfc69697f3f5c64f82
Lirihiwa <lirihiwa@gmail.com>
2024-09-26 15:32:28 +0500

    Task02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,10 +3,27 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        try {
+            byte num = Byte.parseByte(input);
+            return "byte";
+        } catch (NumberFormatException e1) {
+            try {
+                short num = Short.parseShort(input);
+                return "short";
+            } catch (NumberFormatException e2) {
+                try {
+                    int num = Integer.parseInt(input);
+                    return "int";
+                } catch (NumberFormatException e3) {
+                    try {
+                        long num = Long.parseLong(input);
+                        return "long";
+                    } catch (NumberFormatException e4) {
+                        return "число не подходит";
+                    }
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -14,6 +31,7 @@ public class Task02 {
         // например вот так:
         /*
         String result = solution("12345");
+        String result = solution("5");
         System.out.println(result);
          */
     }
